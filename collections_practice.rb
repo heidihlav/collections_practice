7bcb542822ddd184b4ba0dcd708e4d70147471d3
Heidi Hlavinka <hchlav@gmail.com>
2022-02-05 15:17:48 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |x, y| 
+    y <=> x
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |word|
+    word.length
+  end
+end
